7d6a5dbfc725db2bf6bb233ad6ce45185f15af97
ByteVoid <es.yuviz19@gmail.com>
2026-04-10 16:34:30 +0530

    search through topic as well as name of the room

--- base/views.py
+++ base/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
 def home(request):
     q = request.GET.get('q','')
 
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q)
+    )
     topics = Topic.objects.all()
     context = {
         'rooms': rooms,
